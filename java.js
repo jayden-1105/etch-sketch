023cdd84200f7358e378bb192ee07e57bf9f8805
Jayden Jones <jones.jaydenm@gmail.com>
2022-04-06 18:57:27 -0400

    Expanded grid to be 50x50. Added Event Listener Updated the loop and css so that a grid of 50x50 was created rather than the 4x4. Then added in an event listener that changed the color of the square when the mouse passed over it.

--- java.js
+++ java.js
@@ -1,7 +1,18 @@
 mainContainer = document.querySelector('.main');
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 2500; i++) {
     let newDiv = document.createElement('div');
     newDiv.classList.add('div');
     mainContainer.appendChild(newDiv);
 }
+
+
+
+const pixels = document.querySelectorAll('.div');
+
+
+pixels.forEach(pixel => {
+    pixel.addEventListener('mouseover', (e)=>{
+        pixel.classList.add('drawn');
+    });
+});
