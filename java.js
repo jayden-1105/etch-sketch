f4629e038e496161586837dfd9740c72f4f86705
Jayden Jones <jones.jaydenm@gmail.com>
2022-04-06 21:54:11 -0400

    Added functionality to Draw button. Updated some CSS.

--- java.js
+++ java.js
@@ -1,25 +1,39 @@
-mainContainer = document.querySelector('.grid');
+const mainContainer = document.querySelector('.grid');
+const clear = document.querySelector('.clear');
+const draw = document.querySelector('.draw');
+const erase = document.querySelector('.erase');
 
-for (let i = 0; i < 2500; i++) {
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('div');
-    mainContainer.appendChild(newDiv);
+function loadGrid() {
+    for (let i = 0; i < 2500; i++) {
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('div');
+        mainContainer.appendChild(newDiv);
+    }
 }
 
-
-
+loadGrid();
 const pixels = document.querySelectorAll('.div');
 
-
-pixels.forEach(pixel => {
-    pixel.addEventListener('mouseover', (e)=>{
-        pixel.classList.add('drawn');
+draw.addEventListener('click', (e)=> {
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseover', (e)=>{
+            pixel.classList.add('drawn');
+        });
     });
 });
-
-const clear = document.querySelector('.clear');
 
 clear.addEventListener('click', (e)=> {
     pixels.forEach(pixel => { pixel.classList.remove('drawn')
     });
 });
+
+erase.addEventListener('click', (e) => {
+    pixels.forEach(pixel => {
+        pixel.addEventListener('mouseover', (e)=>{
+            pixel.classList.remove('drawn');
+        });
+    });
+});
+
+
+
