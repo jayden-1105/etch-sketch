bbccf173361c6e29dec6d3dc1568f05c7ea7f0cc
Jayden Jones <jones.jaydenm@gmail.com>
2022-04-07 22:34:13 -0400

    Added a lot of functions. Should have committed earlier. worked out the java so I can switch between modes for coloring the grid. Essentially I set up an event listener on each pixel on creation and had them do a function change color when the even happens. the change color function checks the current mode and then based on that sets the target background color.

--- java.js
+++ java.js
@@ -2,24 +2,61 @@ const grid = document.querySelector('.grid');
 const clear = document.querySelector('.clear');
 const draw = document.querySelector('.draw');
 const erase = document.querySelector('.eraser');
+let currentColor = "black";
+let currentMode = "color"
 
+erase.onclick = () => {currentMode = 'erase'};
+draw.onclick = () => {currentMode = 'color'};
+clear.onclick = () => resetGrid();
+
+//Creates the Grid 
 function loadGrid() {
     for (let i = 0; i < 400; i++) {
         let pixel = document.createElement('div');
         pixel.classList.add('pixel');
+        pixel.addEventListener('mouseover', changeColor)
         grid.appendChild(pixel);
+    };
+}
+
+
+function changeColor(e) {
+    if (currentMode === 'color') {
+        e.target.style.backgroundColor = 'black';
+    }
+    else if (currentMode === 'erase') {
+        e.target.style.backgroundColor = 'white';
     }
 }
 
-loadGrid();
-const pixels = document.querySelectorAll('.pixel');
-
-function listen() {
-    pixels.forEach(pixel => {
-        pixel.addEventListener('mouseover', (e) => {
-            pixel.classList.add('drawn');
-        });
-    });
+function resetGrid() {
+    grid.innerHTML = '';
+    loadGrid();
 }
 
-listen();
+window.onload = () => {
+    loadGrid();
+}
+
+
+
+
+//listner function that will perform a certain action based on currentColor
+// function listen() { 
+//     if (currentColor === 'black') {
+//         pixels.forEach(pixel => {
+//             pixel.addEventListener('mouseover', (e) => {
+//                 e.target.style.backgroundColor = "black";
+//                 //pixel.classList.add('drawn');
+//             });
+//         });
+//     }
+//     if (currentColor === 'white') {
+//         pixels.forEach(pixel => {
+//             pixel.addEventListener('mouseover', (e) => {
+//                 e.target.style.backgroundColor = "blue";
+//             });
+//         });
+//     }
+// }
+
